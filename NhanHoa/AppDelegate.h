ed0ade067b4b2bbd51b23f4eeb02be706bf6442e
Khai Le <khai.le@softfoundry.com>
2019-04-23 10:39:59 +0700

    First commit

---
+++ NhanHoa/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NhanHoa
+//
+//  Created by lam quang quan on 4/23/19.
+//  Copyright © 2019 Nhan Hoa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
