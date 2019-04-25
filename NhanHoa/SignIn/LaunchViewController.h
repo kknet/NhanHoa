737ad2e670d61ab4e7326f0cda83adc00f76b866
Khai Le <khai.le@softfoundry.com>
2019-04-25 15:23:38 +0700

    add pch file

---
+++ NhanHoa/SignIn/LaunchViewController.h
@@ -0,0 +1,13 @@
+//
+//  LaunchViewController.h
+//  NhanHoa
+//
+//  Created by lam quang quan on 4/25/19.
+//  Copyright © 2019 Nhan Hoa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LaunchViewController : UIViewController
+
+@end
